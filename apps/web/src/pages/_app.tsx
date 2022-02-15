a490b9cfcbbd5890b605c42f035212e0ba209139
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-15 22:57:11 +0100

    created coplor palette

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,16 +1,38 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffffff',
+      light: '#fffff',
+      dark: '#B2B2B2',
+    },
+    secondary: {
+      main: '#f5de0d',
+      light: '#F7E43D',
+      dark: '#AB9B09',
+    },
+    background: {
+      default: '#23272a',
+      paper: '#2c2f33',
+    },
+  },
+});
+
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Welcome to web!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Welcome to web!</title>
+        </Head>
+        <main className="app">
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
     </>
   );
 }
