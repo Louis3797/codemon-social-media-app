0ff77c2573933a4578c3c370edfa4e5f2c01775e
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-16 21:43:55 +0100

    removed Leftpanel

--- libs/ui/src/lib/layout/LeftPanel.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Container from '@mui/material/Container';
-
-export const LeftPanel: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
-};
