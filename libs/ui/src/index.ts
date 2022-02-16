1b22a3dcf3bbfeefa4843c8da27107076e303de9
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-16 21:44:15 +0100

    removed LeftPanel

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,3 @@
+export * from './Navbar/Navbar';
 export { MainLayout } from './lib/layout/MainLayout';
 export { RightPanel } from './lib/layout/RightPanel';
-export { LeftPanel } from './lib/layout/LeftPanel';
