2388c31c4d5d4b1354ecaef435823c6450f0c018
Louis <53270473+Louis3797@users.noreply.github.com>
2022-02-16 21:43:45 +0100

    removed is2Cols

--- libs/shared-hooks/src/lib/useScreenType.ts
+++ libs/shared-hooks/src/lib/useScreenType.ts
@@ -1,12 +1,8 @@
 import { useMediaQuery } from 'react-responsive';
 
 export const useScreenType = () => {
-  const is2Cols = useMediaQuery({ minWidth: 1265 });
-  const is1Cols = useMediaQuery({ minWidth: 800 });
+  const is1Cols = useMediaQuery({ minWidth: 599 });
 
-  if (is2Cols) {
-    return '2-cols';
-  }
   if (is1Cols) {
     return '1-cols';
   }
